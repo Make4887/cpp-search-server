04c9f0c637a59663fab82a2aa41cc875566a4a97
mihail.kozlov <kozlovmd@student.bmstu.ru>
2023-10-24 19:55:15 +0300

    Corrected fourth sprint

--- search-server/request_queue.h
+++ search-server/request_queue.h
@@ -10,12 +10,7 @@ public:
     explicit RequestQueue(const SearchServer& search_server);
 
     template <typename DocumentPredicate>
-    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
-        std::vector<Document> result = search_server_.FindTopDocuments(raw_query, document_predicate);
-        QueryResult query_result = { result.empty() };
-        AddRequest(query_result);
-        return result;
-    }
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate);
 
     std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status);
 
@@ -35,3 +30,11 @@ private:
 
     void AddRequest(QueryResult query_result);
 };
+
+template <typename DocumentPredicate>
+std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
+    std::vector<Document> result = search_server_.FindTopDocuments(raw_query, document_predicate);
+    QueryResult query_result = { result.empty() };
+    AddRequest(query_result);
+    return result;
+}
